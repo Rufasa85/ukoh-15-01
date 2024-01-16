0642544bc088385ee0f44dcdc7124a6ec42c3d42
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-01-16 14:42:15 -0800

    updates

--- solved.js
+++ solved.js
@@ -22,8 +22,47 @@ FizzBuzz
 ...
 */
 
-function fizzBuzz(){
+function fizzBuzz(endNum){
     //YOUR CODE HERE
+    for(var i = 1; i<=endNum;i++){
+        if(i%3===0 && i%5===0){
+            console.log("FizzBuzz")
+        } else if( i%5===0){
+            console.log("Buzz")
+        }else if( i%3===0){
+            console.log("Fizz")
+        } else {
+            console.log(i)
+        }
+    }
 }
 
-fizzBuzz();
+// function fizzBuzz(endNum) {
+//   for (var i = 1; i <= endNum; i++) {
+//     var result = '';
+//     if (i % 3 === 0) {
+//       result+="Fizz"
+//     }
+//     if (i % 5 === 0) {
+//      result+="Buzz"
+//     }
+//     if(result===""){
+//         result = i
+//     }
+//     console.log(result)
+//   }
+// }
+
+function fizzBuzz(endNum){
+    Array(endNum).fill(null).forEach(function(e,i){console.log((i+1)%15===0?"FizzBuzz":(i+1)%5===0?"Buzz":(i+1)%3===0?"Fizz":i+1)})
+}
+
+fizzBuzz(50);
+
+var joe = true;
+// if(joe===true){
+//     console.log("its true")
+// } else {
+//     console.log("its false;")
+// }
+joe===true? console.log("its true"):console.log("its false")
