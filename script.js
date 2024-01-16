1391d2fb3e1fc51e2b197540d0333fc13048271d
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-01-16 11:08:04 -0800

    wip

---
+++ script.js
@@ -0,0 +1,29 @@
+//write a function that logs all the numbers from 1-100 with the following exceptions:
+// if the number is divisible by 3, log "Fizz" instead.
+// if the number is divisible by 5, log "Buzz" instead.
+// of the number is divisible by 3 and 5, log "FizzBuzz" instead.
+
+/*output
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+...
+*/
+
+function fizzBuzz(){
+    //YOUR CODE HERE
+}
+
+fizzBuzz();
